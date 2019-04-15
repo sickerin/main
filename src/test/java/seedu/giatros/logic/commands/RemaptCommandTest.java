aa7760ae7a0bfd00f69e656e0ed8692f54fac5cf
Daniel Pyone Maung <c.danpm@gmail.com>
2019-04-15 13:15:50 +0800

    add RemaptCommand related tests
    - modified typical patients idx 1 with appointment to avoid significant refactoring of tests
    - modified RemallCommandTest with appointments
    - add appointments to PatientCardHandle

--- src/test/java/seedu/giatros/logic/commands/RemaptCommandTest.java
+++ src/test/java/seedu/giatros/logic/commands/RemaptCommandTest.java
@@ -2,8 +2,8 @@ package seedu.giatros.logic.commands;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static seedu.giatros.logic.commands.CommandTestUtil.VALID_ALLERGY_AMY;
-import static seedu.giatros.logic.commands.CommandTestUtil.VALID_ALLERGY_BOB;
+import static seedu.giatros.logic.commands.CommandTestUtil.VALID_APPOINTMENT_YMDH;
+import static seedu.giatros.logic.commands.CommandTestUtil.VALID_APPOINTMENT_YMDHM;
 import static seedu.giatros.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.giatros.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.giatros.logic.commands.CommandTestUtil.showPatientAtIndex;
@@ -22,41 +22,41 @@ import seedu.giatros.model.GiatrosBook;
 import seedu.giatros.model.Model;
 import seedu.giatros.model.ModelManager;
 import seedu.giatros.model.UserPrefs;
-import seedu.giatros.model.allergy.Allergy;
+import seedu.giatros.model.appointment.Appointment;
 import seedu.giatros.model.patient.Patient;
 
-public class RemallCommandTest {
+public class RemaptCommandTest {
 
     private Model model = new ModelManager(getTypicalGiatrosBook(), new UserPrefs());
     private CommandHistory commandHistory = new CommandHistory();
 
     @Test
-    public void execute_removeAllergyUnfilteredList_success() {
+    public void execute_removeAppointmentUnfilteredList_success() {
         Patient firstPatient = model.getFilteredPatientList().get(INDEX_FIRST_PATIENT.getZeroBased());
         Patient editedPatient = new Patient(firstPatient.getName(), firstPatient.getPhone(), firstPatient.getEmail(),
-                firstPatient.getAddress(), new HashSet<>(), firstPatient.getAppointments());
+                firstPatient.getAddress(), firstPatient.getAllergies(), new HashSet<>());
+        
+        RemaptCommand remaptCommand = new RemaptCommand(INDEX_FIRST_PATIENT, firstPatient.getAppointments());
 
-        RemallCommand remallCommand = new RemallCommand(INDEX_FIRST_PATIENT, firstPatient.getAllergies());
-
-        String expectedMessage = String.format(RemallCommand.MESSAGE_REMOVE_ALLERGY_SUCCESS, editedPatient);
+        String expectedMessage = String.format(RemaptCommand.MESSAGE_REMOVE_APPOINTMENT_SUCCESS, editedPatient);
 
         Model expectedModel = new ModelManager(new GiatrosBook(model.getGiatrosBook()), new UserPrefs());
         expectedModel.setPatient(firstPatient, editedPatient);
         expectedModel.commitGiatrosBook();
 
-        assertCommandSuccess(remallCommand, model, commandHistory, expectedMessage, expectedModel);
+        assertCommandSuccess(remaptCommand, model, commandHistory, expectedMessage, expectedModel);
     }
 
     @Test
-    public void execute_removeNonExistentAllergyUnfilteredList_failure() {
+    public void execute_removeNonExistentAppointmentUnfilteredList_failure() {
         Patient firstPatient = model.getFilteredPatientList().get(INDEX_FIRST_PATIENT.getZeroBased());
-        Allergy nonExistentAllergy = new Allergy("randomAllergy");
-        RemallCommand remallCommand = new RemallCommand(INDEX_FIRST_PATIENT, nonExistentAllergy);
-        // ensures that nonExistentAllergy is indeed not found in first patient
-        assertFalse(firstPatient.getAllergies().contains(nonExistentAllergy));
+        Appointment nonExistentAppointment = new Appointment(VALID_APPOINTMENT_YMDH);
+        RemaptCommand remaptCommand = new RemaptCommand(INDEX_FIRST_PATIENT, nonExistentAppointment);
+        // ensures that nonExistentAppointment is indeed not found in first patient
+        assertFalse(firstPatient.getAppointments().contains(nonExistentAppointment));
 
-        assertCommandFailure(remallCommand, model, commandHistory,
-                String.format(RemallCommand.MESSAGE_REMOVE_ALLERGY_FAILURE, firstPatient));
+        assertCommandFailure(remaptCommand, model, commandHistory,
+                String.format(RemaptCommand.MESSAGE_REMOVE_APPOINTMENT_FAILURE, firstPatient));
     }
 
     @Test
@@ -65,25 +65,25 @@ public class RemallCommandTest {
 
         Patient firstPatient = model.getFilteredPatientList().get(INDEX_FIRST_PATIENT.getZeroBased());
         Patient editedPatient = new Patient(firstPatient.getName(), firstPatient.getPhone(), firstPatient.getEmail(),
-                firstPatient.getAddress(), new HashSet<>(), firstPatient.getAppointments());
+                firstPatient.getAddress(), firstPatient.getAllergies(), new HashSet<>());
 
-        RemallCommand remallCommand = new RemallCommand(INDEX_FIRST_PATIENT, firstPatient.getAllergies());
+        RemaptCommand remaptCommand = new RemaptCommand(INDEX_FIRST_PATIENT, firstPatient.getAppointments());
 
-        String expectedMessage = String.format(RemallCommand.MESSAGE_REMOVE_ALLERGY_SUCCESS, editedPatient);
+        String expectedMessage = String.format(RemaptCommand.MESSAGE_REMOVE_APPOINTMENT_SUCCESS, editedPatient);
 
         Model expectedModel = new ModelManager(new GiatrosBook(model.getGiatrosBook()), new UserPrefs());
         expectedModel.setPatient(firstPatient, editedPatient);
         expectedModel.commitGiatrosBook();
 
-        assertCommandSuccess(remallCommand, model, commandHistory, expectedMessage, expectedModel);
+        assertCommandSuccess(remaptCommand, model, commandHistory, expectedMessage, expectedModel);
     }
 
     @Test
     public void execute_invalidPatientIndexUnfilteredList_failure() {
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPatientList().size() + 1);
-        RemallCommand remallCommand = new RemallCommand(outOfBoundIndex, new Allergy(VALID_ALLERGY_BOB));
+        RemaptCommand remaptCommand = new RemaptCommand(outOfBoundIndex, new Appointment(VALID_APPOINTMENT_YMDHM));
 
-        assertCommandFailure(remallCommand, model, commandHistory, Messages.MESSAGE_INVALID_PATIENT_DISPLAYED_INDEX);
+        assertCommandFailure(remaptCommand, model, commandHistory, Messages.MESSAGE_INVALID_PATIENT_DISPLAYED_INDEX);
     }
 
     /**
@@ -97,25 +97,25 @@ public class RemallCommandTest {
         // ensures that outOfBoundIndex is still in bounds of giatros book list
         assertTrue(outOfBoundIndex.getZeroBased() < model.getGiatrosBook().getPatientList().size());
 
-        RemallCommand remallCommand = new RemallCommand(outOfBoundIndex, new Allergy(VALID_ALLERGY_BOB));
+        RemaptCommand remaptCommand = new RemaptCommand(outOfBoundIndex, new Appointment(VALID_APPOINTMENT_YMDHM));
 
-        assertCommandFailure(remallCommand, model, commandHistory, Messages.MESSAGE_INVALID_PATIENT_DISPLAYED_INDEX);
+        assertCommandFailure(remaptCommand, model, commandHistory, Messages.MESSAGE_INVALID_PATIENT_DISPLAYED_INDEX);
     }
 
     @Test
     public void executeUndoRedo_validIndexUnfilteredList_success() throws Exception {
         Patient patientToModify = model.getFilteredPatientList().get(INDEX_FIRST_PATIENT.getZeroBased());
         Patient modifiedPatient = new Patient(patientToModify.getName(), patientToModify.getPhone(),
-                patientToModify.getEmail(), patientToModify.getAddress(), new HashSet<>(), patientToModify.getAppointments());
+                patientToModify.getEmail(), patientToModify.getAddress(), patientToModify.getAllergies(), new HashSet<>());
 
-        RemallCommand remallCommand = new RemallCommand(INDEX_FIRST_PATIENT, patientToModify.getAllergies());
+        RemaptCommand remaptCommand = new RemaptCommand(INDEX_FIRST_PATIENT, patientToModify.getAppointments());
 
         Model expectedModel = new ModelManager(model.getGiatrosBook(), new UserPrefs());
         expectedModel.setPatient(patientToModify, modifiedPatient);
         expectedModel.commitGiatrosBook();
 
-        // remall -> first patient allergy changed
-        remallCommand.execute(model, commandHistory);
+        // remapt -> first patient appointment changed
+        remaptCommand.execute(model, commandHistory);
 
         // undo -> reverts giatrosbook back to previous state and filtered patient list to show all patients
         expectedModel.undoGiatrosBook();
@@ -129,10 +129,10 @@ public class RemallCommandTest {
     @Test
     public void executeUndoRedo_invalidIndexUnfilteredList_failure() {
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPatientList().size() + 1);
-        RemallCommand remallCommand = new RemallCommand(outOfBoundIndex, new Allergy("randomAllergy"));
+        RemaptCommand remaptCommand = new RemaptCommand(outOfBoundIndex, new Appointment(VALID_APPOINTMENT_YMDH));
 
         // execution failed -> giatros book state not added into model
-        assertCommandFailure(remallCommand, model, commandHistory, Messages.MESSAGE_INVALID_PATIENT_DISPLAYED_INDEX);
+        assertCommandFailure(remaptCommand, model, commandHistory, Messages.MESSAGE_INVALID_PATIENT_DISPLAYED_INDEX);
 
         // single giatros book state in model -> undoCommand and redoCommand fail
         assertCommandFailure(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_FAILURE);
@@ -140,7 +140,7 @@ public class RemallCommandTest {
     }
 
     /**
-     * 1. Modifies {@code Patient#allergy} from a filtered list.
+     * 1. Modifies {@code Patient#appointment} from a filtered list.
      * 2. Undo the modification.
      * 3. The unfiltered list should be shown now. Verify that the index of the previously modified patient in the
      * unfiltered list is different from the index at the filtered list.
@@ -152,17 +152,17 @@ public class RemallCommandTest {
 
         Patient patientToModify = model.getFilteredPatientList().get(INDEX_FIRST_PATIENT.getZeroBased());
         Patient modifiedPatient = new Patient(patientToModify.getName(), patientToModify.getPhone(),
-                patientToModify.getEmail(), patientToModify.getAddress(), new HashSet<>(),
-                patientToModify.getAppointments());
+                patientToModify.getEmail(), patientToModify.getAddress(), patientToModify.getAllergies(),
+                new HashSet<>());
 
-        RemallCommand remallCommand = new RemallCommand(INDEX_FIRST_PATIENT, patientToModify.getAllergies());
+        RemaptCommand remaptCommand = new RemaptCommand(INDEX_FIRST_PATIENT, patientToModify.getAppointments());
         Model expectedModel = new ModelManager(model.getGiatrosBook(), new UserPrefs());
 
         expectedModel.setPatient(patientToModify, modifiedPatient);
         expectedModel.commitGiatrosBook();
 
-        // remall -> modifies second patient in unfiltered patient list / first patient in filtered patient list
-        remallCommand.execute(model, commandHistory);
+        // remapt -> modifies second patient in unfiltered patient list / first patient in filtered patient list
+        remaptCommand.execute(model, commandHistory);
 
         // undo -> reverts giatrosbook back to previous state and filtered patient list to show all patients
         expectedModel.undoGiatrosBook();
@@ -175,10 +175,10 @@ public class RemallCommandTest {
 
     @Test
     public void equals() {
-        final RemallCommand standardCommand = new RemallCommand(INDEX_FIRST_PATIENT, new Allergy(VALID_ALLERGY_AMY));
+        final RemaptCommand standardCommand = new RemaptCommand(INDEX_FIRST_PATIENT, new Appointment(VALID_APPOINTMENT_YMDH));
 
         // same values -> returns true
-        RemallCommand commandWithSameValues = new RemallCommand(INDEX_FIRST_PATIENT, new Allergy(VALID_ALLERGY_AMY));
+        RemaptCommand commandWithSameValues = new RemaptCommand(INDEX_FIRST_PATIENT, new Appointment(VALID_APPOINTMENT_YMDH));
         assertTrue(standardCommand.equals(commandWithSameValues));
 
         // same object -> returns true
@@ -191,10 +191,10 @@ public class RemallCommandTest {
         assertFalse(standardCommand.equals(new ClearCommand()));
 
         // different index -> returns false
-        assertFalse(standardCommand.equals(new RemallCommand(INDEX_SECOND_PATIENT, new Allergy(VALID_ALLERGY_AMY))));
+        assertFalse(standardCommand.equals(new RemaptCommand(INDEX_SECOND_PATIENT, new Appointment(VALID_APPOINTMENT_YMDH))));
 
         // different descriptor -> returns false
-        assertFalse(standardCommand.equals(new RemallCommand(INDEX_FIRST_PATIENT, new Allergy(VALID_ALLERGY_BOB))));
+        assertFalse(standardCommand.equals(new RemaptCommand(INDEX_FIRST_PATIENT, new Appointment(VALID_APPOINTMENT_YMDHM))));
     }
 
 }
